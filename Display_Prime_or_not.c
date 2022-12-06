df53603c3bff9823a147e2051a43ac9c5759dcac
Haresh <kuradeharesh4002@gmail.com>
2022-12-06 14:21:49 +0530

    New lines added

--- Display_Prime_or_not.c
+++ Display_Prime_or_not.c
@@ -1,28 +1,34 @@
-//Program Of C To Determine The Number is Prime Or not
-//Created By Haresh Kurade, FW20CO002.
+// Program Of C To Determine The Number is Prime Or not
+// Created By Haresh Kurade, FW20CO002.
+
 #include <stdio.h>
-int main() 
+
+int main()
 {
     int n, i, b = 0;
+
     printf("Enter a positive Number: ");
+
     scanf("%d", &n);
 
-    for (i = 2; i <= n / 2; i++) 
+    for (i = 2; i <= n / 2; i++)
     {
-        if (n % i == 0) 
+        if (n % i == 0)
         {
             b = 1;
         }
     }
 
-    if (n == 1) 
+    if (n == 1)
     {
         printf("1 is neither prime nor composite.");
     }
-    else 
+
+    else
     {
         if (b == 0)
             printf("%d is a prime number.", n);
+
         else
             printf("%d is not a prime number.", n);
     }
